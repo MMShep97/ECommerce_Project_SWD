aea22b6d79acbb62efaca0e432cdf0fab9c1cf02
Daniel Kelly <daniel-kelly-1@uiowa.edu>
2018-12-01 14:28:28 -0600

    made a test file, working on login page

---
+++ ECommerce_Project/src/Daniel_test.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Daniel_test {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Login");
+        PageLogin pl = new PageLogin();
+        frame.add(pl);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1080, 720);
+        frame.setVisible(true);
+    }
+}
--- ECommerce_Project/src/ECommmerceClient.java
+++ ECommerce_Project/src/ECommmerceClient.java
@@ -1,81 +1,81 @@
-import util.ECommerceUtilityMethods;
-
-import javax.swing.*;
-import java.io.*;
-import java.net.InetAddress;
-import java.net.Socket;
-import static util.ECommerceUtilityMethods.*;
-
-public class ECommmerceClient extends JFrame
-{
-    //Network members
-    private ObjectOutputStream output;
-    private ObjectInputStream input;
-    private Socket client;
-    private String host;
-
-    //GUI components/parameters
-    private int browsePageCapacity = 8;
-
-    public ECommmerceClient(String host)
-    {
-        this.host = host;
-    }
-
-    public void runClient()
-    {
-        try
-        {
-            //Connect to server
-            disp("Attempting to connect");
-            client = new Socket(InetAddress.getLocalHost(), ECommerceServer.PORT);
-            disp("Connected to: " + client.getInetAddress().getHostName());
-
-            //Get I/O streams
-            output = new ObjectOutputStream(client.getOutputStream());
-            output.flush();
-            input = new ObjectInputStream(client.getInputStream());
-
-            //Interact with server
-            interact();
-        }
-        catch (EOFException eof)
-        {
-            disp();
-        }
-        catch (IOException ioe)
-        {
-
-        }
-        finally
-        {
-            disp("Closing connection");
-            closeConnections(output, input, client);
-        }
-    }
-
-    private void interact()
-    {
-        boolean interact = true;
-
-        do
-        {
-            try
-            {
-
-            }
-            catch ()
-            {
-
-            }
-
-        }while(interact)
-    }
-
-
-    //TODO: Display to sepcific GUI area (maybe using invokelater)
-    private void toGUI(final String message)
-    {
-
-    }
-}
+//import util.ECommerceUtilityMethods;
+//
+//import javax.swing.*;
+//import java.io.*;
+//import java.net.InetAddress;
+//import java.net.Socket;
+//import static util.ECommerceUtilityMethods.*;
+//
+//public class ECommmerceClient extends JFrame
+//{
+//    //Network members
+//    private ObjectOutputStream output;
+//    private ObjectInputStream input;
+//    private Socket client;
+//    private String host;
+//
+//    //GUI components/parameters
+//    private int browsePageCapacity = 8;
+//
+//    public ECommmerceClient(String host)
+//    {
+//        this.host = host;
+//    }
+//
+//    public void runClient()
+//    {
+//        try
+//        {
+//            //Connect to server
+//            disp("Attempting to connect");
+//            client = new Socket(InetAddress.getLocalHost(), ECommerceServer.PORT);
+//            disp("Connected to: " + client.getInetAddress().getHostName());
+//
+//            //Get I/O streams
+//            output = new ObjectOutputStream(client.getOutputStream());
+//            output.flush();
+//            input = new ObjectInputStream(client.getInputStream());
+//
+//            //Interact with server
+//            interact();
+//        }
+//        catch (EOFException eof)
+//        {
+//            disp();
+//        }
+//        catch (IOException ioe)
+//        {
+//
+//        }
+//        finally
+//        {
+//            disp("Closing connection");
+//            closeConnections(output, input, client);
+//        }
+//    }
+//
+//    private void interact()
+//    {
+//        boolean interact = true;
+//
+//        do
+//        {
+//            try
+//            {
+//
+//            }
+//            catch ()
+//            {
+//
+//            }
+//
+//        }while(interact)
+//    }
+//
+//
+//    //TODO: Display to sepcific GUI area (maybe using invokelater)
+//    private void toGUI(final String message)
+//    {
+//
+//    }
+//}
