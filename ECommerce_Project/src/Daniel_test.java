ae9423a6ba6e3a840f12aa8f31fc089b2a6b2397
Daniel Kelly <daniel-kelly-1@uiowa.edu>
2018-12-01 14:44:24 -0600

    login

--- ECommerce_Project/src/Daniel_test.java
+++ ECommerce_Project/src/Daniel_test.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Daniel_test {
     public static void main(String[] args){
-        JFrame frame = new JFrame("Login");
+        JFrame frame = new JFrame("O B E Y");
         PageLogin pl = new PageLogin();
         frame.add(pl);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- ECommerce_Project/src/PageLogin.java
+++ ECommerce_Project/src/PageLogin.java
@@ -3,23 +3,19 @@ import java.awt.*;
 
 public class PageLogin extends JPanel {
 
-    private final JPanel panel;
     private final JButton button;
     private final JTextField usernameField;
     private final JTextField passwordField;
 
     public PageLogin() {
-        panel = new JPanel();
         button = new JButton("LOGIN");
         usernameField = new JTextField("Username");
         passwordField = new JTextField("Password");
         usernameField.setEditable(true);
         passwordField.setEditable(true);
-        panel.setLayout(new BorderLayout());
-        panel.add(button, BorderLayout.SOUTH);
-        panel.add(usernameField, BorderLayout.NORTH);
-        panel.add(passwordField, BorderLayout.CENTER);
-
-        add(panel);
+        setLayout(new BorderLayout());
+        add(button, BorderLayout.SOUTH);
+        add(usernameField, BorderLayout.NORTH);
+        add(passwordField, BorderLayout.CENTER);
     }
 }
