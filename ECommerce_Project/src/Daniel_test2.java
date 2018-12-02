f18b3916950ce185caf68650080514317c3e3677
Daniel Kelly <daniel-kelly-1@uiowa.edu>
2018-12-01 21:59:00 -0600

    sterf

--- ECommerce_Project/src/Daniel_test2.java
+++ ECommerce_Project/src/Daniel_test2.java
@@ -3,8 +3,10 @@ import javax.swing.*;
 public class Daniel_test2 {
     public static void main(String[] args){
         ECommerceClient client = new ECommerceClient("a");
+        client.initializeGUI();
         NavigationBar navbar = new NavigationBar(client);
         PageLogin pl = new PageLogin(client, navbar);
+        client.getContentPane().removeAll();
         client.add(pl);
         client.setVisible(true);
     }
