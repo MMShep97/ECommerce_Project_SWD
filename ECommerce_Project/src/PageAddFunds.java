a26aaca8bc56f6bd21fddb4103a8e1a11822edae
Daniel Kelly <daniel-kelly-1@uiowa.edu>
2018-12-02 21:08:26 -0600

    did it work

--- ECommerce_Project/src/PageAddFunds.java
+++ ECommerce_Project/src/PageAddFunds.java
@@ -5,13 +5,29 @@ import java.awt.event.ActionListener;
 
 import static util.PageUtilityMethods.createLogoPanel;
 
+/*
+ * Represents a page where users add funds to their accounts. extends JPanel. Is added to ECommerceClient object in
+ * GUI to display the page
+ */
 public class PageAddFunds extends JPanel
 {
+    /*
+     * client is the ECommerceClient object used to communicate with the server, and also functions as the frame that
+     *      holds this PageAddFunds object
+     * navBar is the NavigationBar object in this GUI
+     * inputFundsField is the field where users input the amount of funding they want to add to their account
+     * addFundsButton is a JButton users press to actually add the funds. There is an actionlistener attached to it
+     */
     private ECommerceClient client;
     private NavigationBar navBar;
     private JTextField inputFundsField;
     private JButton addFundsButton;
 
+    /*
+     * Constructor configures the PageAddFunds panel with inputFundsField and addFundsButton
+     * @param client the ECommerceClient object passed in and assigned to client
+     * @param navBar the NavigationBar object passed in and assigned to NavBar
+     */
     public PageAddFunds(ECommerceClient client, NavigationBar navBar)
     {
         super(new BorderLayout());
@@ -39,11 +55,18 @@ public class PageAddFunds extends JPanel
         add(wrapper, BorderLayout.CENTER);
     }
 
+    /*
+     * if funds were unable to be added, reports an error message
+     */
     public void unsuccessful()
     {
         inputFundsField.setText("An Error Has Occurred");
     }
 
+    /*
+     * The action listener class for the addFundsButton. Overrides actionPerformed, communicates with server telling it
+     * to add specified amount of funds to user's account
+     */
     private class AddFundsButtonListener implements ActionListener
     {
         @Override
